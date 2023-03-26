03962d52b15d04bb20f4cb1892059f88fac61766
Luciano Melhem <lucianomelhem@hotmail.com>
2023-03-26 16:43:39 -0300

    resolved some errors when running npm run build

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Link from "next/link";
 
 const Gallery = () => {
-    const [fileContents, setFileContents] = useState(null);
+    const [fileContents, setFileContents] = useState<string[] | null>(null);
     console.log("hellohellohellohellohellohellohellohellohellohellohellohellohellohellohello")
     useEffect(() => {
         async function fetchFileContents() {
@@ -15,11 +15,12 @@ const Gallery = () => {
     if (!fileContents) {
         return <div className="white">Loading...</div>;
     }
+    // @ts-ignore
     return (
         <div>
-            <h1 className="main_title">Project's Gallery - <Link href={"/"}>turn back</Link></h1>
+            <h1 className="main_title">Project&apos;s Gallery - <Link href={"/"}>turn back</Link></h1>
             <div className="container">
-                {fileContents.map(img=><span className="m-3"><img key={Math.random()} src={"proyect_gifs/"+img} width={200} ></img></span>)}
+                {fileContents.map(img=><span className="m-3" key={Math.random()}><img  src={"proyect_gifs/"+img} width={200} alt={img} ></img></span>)}
             </div>
 
         </div>
