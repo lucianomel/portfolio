63b453085496dafaab448e8c3e8129ff01f1ccdf
Luciano Melhem <lucianomelhem@hotmail.com>
2023-03-26 09:45:37 -0300

    with PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['upload.wikimedia.org'],
-  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    sw: '/sw.js',
+    disable: process.env.NODE_ENV === 'development',
+    // Add an icon for your PWA
+    icons: [
+      {
+        src: '/cv.png',
+        sizes: [72, 96, 128, 144, 192, 256, 384, 512],
+        destination: '/public/icons',
+      },
+    ],
+  }
 };
